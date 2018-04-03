855398774cd288c42d1fb1005f573f25227b8696
ArthurH <arthur.dherbemont@aimms.com>
2018-04-03 13:15:41 +0200

    included custom.css file in conf.py

--- conf.py
+++ conf.py
@@ -118,7 +118,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_context = {
-    'css_files': ['_static/Hacks.css'],
+    'css_files': ['_static/Hacks.css', '_static/custom.css'],
 }
 
 # Custom sidebar templates, must be a dictionary that maps document names
