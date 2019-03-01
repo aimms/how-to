6164d12b25a3ad3b5aa5284327e7ec4115093907
ArthurH <arthur.dherbemont@aimms.com>
2019-03-01 11:45:33 +0100

    added .htaccess to static html

--- conf.py
+++ conf.py
@@ -157,7 +157,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', '.htaccess']
 
 # if builds on GitLab (a Linux machine), force "Edit on Gitlab" not to be shown :)
 if os.name != 'nt':
