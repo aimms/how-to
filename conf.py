d315ebbdc306f55a5896311fdf33a085659b74ae
ArthurH <arthur.dherbemont@aimms.com>
2019-03-26 14:53:56 +0100

    added spelling checker + corrected todos syntax

--- conf.py
+++ conf.py
@@ -39,7 +39,8 @@ extensions = ['sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
-	'sphinx.builders.linkcheck']
+	'sphinx.builders.linkcheck',
+	'sphinxcontrib.spelling']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
