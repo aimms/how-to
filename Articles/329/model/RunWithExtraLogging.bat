08c3c2cf985f6bb85d2e856fa0b2872d72d050ae
Chris Kuip <chris.kuip@aimms.com>
2019-12-24 14:02:12 +0100

    Drafted #329

--- Articles/329/model/RunWithExtraLogging.bat
+++ Articles/329/model/RunWithExtraLogging.bat
@@ -1,18 +1,19 @@
 echo on
-rem Modify this to select the proper AIMMS Version.
-set AIMMSVERSION=4.71.10000.21-x64-VS2017
-rem select the latest AIMMS version
 
+rem Modify this to select the proper AIMMS Version.
+rem set AIMMSVERSION=4.70.2.4-x64-VS2017
+
+rem Select the latest AIMMS version
 rem Get the latest AIMMS release installed by pushing/popping both drive and path, 
 rem and then switching to the folder of installed AIMMS versions.
 rem Assumption: the users folder is on the C drive.
-rem set DRIVEUSEDATSTART=%~d0%
-rem pushd
-rem c:
-rem cd %localappdata%\AIMMS\IFA\Aimms\
-rem for /f "usebackq delims=|" %%f in (`dir /b `) do set AIMMSVERSION=%%f
-rem popd
-rem %DRIVEUSEDATSTART%
+set DRIVEUSEDATSTART=%~d0%
+pushd
+c:
+cd %localappdata%\AIMMS\IFA\Aimms\
+for /f "usebackq delims=|" %%f in (`dir /b `) do set AIMMSVERSION=%%f
+popd
+%DRIVEUSEDATSTART%
 
 set AIMMSEXECUTABLE="%localappdata%\AIMMS\IFA\Aimms\%AIMMSVERSION%\Bin\aimms.exe"
 
@@ -24,8 +25,8 @@ for /f "usebackq delims=|" %%f in (`dir /b *.aimms`) do %AIMMSEXECUTABLE% --logc
 rem Assuming here LoggerConfig.xml still writes to the fixed folder c:\temp,
 rem we move the create log file from this folder to the current folder.
 rem Because of this fixed location, we can only debug one AIMMS project at a time!
-copy c:\temp\aimms-log.* .
-del c:\temp\aimms-log.*
+copy c:\temp\aimms-log.xml log
+del c:\temp\aimms-log.xml
 
 pause
 
