9f0758343995b40f54b62a69341923e4bc06d6b8
Arthur dHerbemont <arthur.dherbemont@aimms.com>
2020-01-01 22:30:30 +0000

    Hotfix: removed version number, since it conflicted with the new sitemap extension release (from december 13)

--- conf.py
+++ conf.py
@@ -73,9 +73,9 @@ author = u'AIMMS'
 # built documents.
 #
 # The short X.Y version.
-version = u'1'
+#version = u'1'
 # The full version, including alpha/beta/rc tags.
-release = u'0'
+#release = u'0'
 
 # includes these texts at the end of all source .rsts, helpful for using repetitive replacements 
 
