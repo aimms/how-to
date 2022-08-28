461756b9249bc3c6b79a606740b80b73e17ea952
Chris Kuip <chris.kuip@aimms.com>
2022-08-28 11:35:21 +0200

    #351 --run-only now support PostMainInitilization and LibraryPostInitialization.

--- Articles/351/model/InitTerm/RunOnly.bat
+++ Articles/351/model/InitTerm/RunOnly.bat
@@ -1,6 +1,6 @@
 echo on
 
-set AIMMSVERSION=4.78.3.18-x64-VS2017
+set AIMMSVERSION=4.87.7.5-x64-VS2017
 
 rem set AIMMSEXECUTABLE="%localappdata%\AIMMS\IFA\Aimms\%AIMMSVERSION%\Bin\aimmscmd.exe"
 set AIMMSEXECUTABLE="%localappdata%\AIMMS\IFA\Aimms\%AIMMSVERSION%\Bin\aimms.exe"
